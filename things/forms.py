57906292179464596978a7d8938a1a27ca205e21
benx9802 <benjamin.ferrey@kcl.ac.uk>
2023-12-08 11:14:42 +0000

    Create Things form and display form in home view

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
-
+from django import forms
+from django.core.validators import MinValueValidator, MaxValueValidator
 # Create your forms here.
+
+class ThingForm(forms.Form):
+
+    name = forms.CharField(max_length=35)
+    description = forms.CharField(max_length=120,widget= forms.Textarea)
+    quantity = forms.IntegerField(validators=[MinValueValidator(0),MaxValueValidator(50)],widget=forms.NumberInput)
--- things/views.py
+++ things/views.py
@@ -1,4 +1,28 @@
 from django.shortcuts import render
+from .forms import ThingForm
+from .models import Thing
+from datetime import datetime
 
 def home(request):
+    
+    if request.method == "POST":
+        form = ThingForm(request.POST)
+        if form.is_valid():
+            print("SAVED")
+            form_input = form.cleaned_data
+            new_thing = Thing(
+                name=form_input['name'],
+                description = form_input['description'],
+                quantity = form_input['quantity']
+            )
+            try:
+                new_thing.save()
+            except:
+                print("ERROR")
+                return render(request, 'home.html', {'form': form})
+        else:
+            print("NONE")
+            return render(request, 'home.html', {'form': form})
+
+    form = ThingForm()
     return render(request, 'home.html', {'form': form})
